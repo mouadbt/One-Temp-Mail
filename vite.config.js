98ce2706389d6c87780f533ddcf0cdfa80f54b44
Mouad <mouadtb.dev@gmail.com>
2024-12-03 22:50:02 +0100

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from "path"
 
 export default defineConfig({
   plugins: [react()],
-  base: "/One-Temp-Mail-V2/",
+  base: "/One-Temp-Mail-2",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
